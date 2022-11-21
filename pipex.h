23aaf956bcf247fbc3299faaee751511556b3c24
jhidalgo42 <jhidalgo@student.42madrid.com>
2022-11-21 18:17:03 +0100

    first files

---
+++ pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhidalgo <jhidalgo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 18:03:38 by jhidalgo          #+#    #+#             */
+/*   Updated: 2022/11/21 18:15:45 by jhidalgo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+
+#endif
